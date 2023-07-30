0f4ed7f91785520c4eb8e281ce299333b2807c13
AnnaBooki <a.buch@stud.uni-heidelberg.de>
2023-07-30 19:51:49 +0200

    added r_evaluation funcs, plot_confusionmatrix, pipelines for discretized xgb and logr

--- utils/utils_figures.py
+++ utils/utils_figures.py
@@ -4,9 +4,11 @@
 
 
 import numpy as np
+import pandas as pd
 
 from statsmodels.stats.outliers_influence import variance_inflation_factor
-from scipy.stats import pearsonr
+from scipy.stats import spearmanr, permutation_test
+from sklearn.metrics import confusion_matrix
 
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
@@ -24,36 +26,61 @@ def plot_scatterplot_3d(df, x, y, z):
     sns.relplot(data=df, x=df[x], y=df[y], hue=df[z]) #aspect=1.61)
 
 
-def plot_pearsoncorrelation(df_sm_corr, signif=True, psig=0.05):
+def plot_spearman_rank(df_corr, min_periods=100, signif=True, psig=0.05):
         """
-        ## Code snippet : https://stackoverflow.com/questions/69900363/colour-statistically-non-significant-values-in-seaborn-heatmap-with-a-different
+        ## Code snippet modified: https://stackoverflow.com/questions/69900363/colour-statistically-non-significant-values-in-seaborn-heatmap-with-a-different
 
-        df_sm_corr (dataframe): dataframe with variables to plot
+        df_corr (dataframe): dataframe with variables to plot
+        min_periods (int): Minimum number of observations required per pair of columns to have a valid result
         signifcance (boolean): should non significant be masked
         psig (float): signifcance level
         return: Figure for Pearson Correlation 
         """ 
-        
+ 
         ## get the p value for pearson coefficient, subtract 1 on the diagonal
-        pvals = df_sm_corr.corr(method=lambda x, y: pearsonr(x, y)[1]) - np.eye(*df_sm_corr.corr().shape)
-        psig = 0.05  # 5% signif level
+        pvals = df_corr.corr(method=lambda x, y: spearmanr(x, y)[1], min_periods=min_periods) - np.eye(*df_corr.corr(method="spearman", min_periods=min_periods).shape)  # np.eye(): diagonal= ones, elsewere=zeros
 
         #  main plot
-        sns.heatmap(df_sm_corr.corr(), annot=False, square=True, cmap="RdBu", fmt=".2f", zorder=1)
+        sns.heatmap(df_corr.corr(method="spearman", min_periods=min_periods), annot=False, square=True, cmap="RdBu", fmt=".2f", zorder=1)
 
         # signifcance mask
         if signif == True:
                 ## add another heatmap with colouring the non-significant cells
-                sns.heatmap(df_sm_corr.corr()[pvals>=psig], annot=False, square=True, cbar=False, 
-
-        ## add-ons
-        cmap=sns.color_palette("Greys", n_colors=1, desat=1),  zorder = 2) #put the map above the heatmap
+                sns.heatmap(df_corr.corr(method="spearman", min_periods=min_periods)[pvals>=psig], 
+                            annot=False, square=True, cbar=False,
+                            ## add-ons
+                            cmap=sns.color_palette("Greys", n_colors=1, desat=1),  
+                            zorder = 2) # put the map above the heatmap
         ## add a label for the colour
         colors = [sns.color_palette("Greys", n_colors=1, desat=1)[0]]
         texts = [f"not significant (at {psig})"]
         patches = [ mpatches.Patch(color=colors[i], label="{:s}".format(texts[i]) ) for i in range(len(texts)) ]
         plt.legend(handles=patches, bbox_to_anchor=(.85, 1.05), loc='center')
 
+
+def plot_confusion_matrix(y_test, y_pred, model_name="", target_name="", pipe_name=""):
+    """ 
+    Plot conufsion matrix
+    return: plot and dataframe
+    """
+    cm = confusion_matrix(y_test, y_pred)
+    cm = pd.DataFrame(cm, 
+        range(len(y_test.unique())), 
+        range(len(y_test.unique()))
+        )
+    plt.figure(figsize=(6,6))
+    sns.set(font_scale=1.5)
+    sns.heatmap(cm, annot=True, 
+        annot_kws={"size":16},
+        fmt="g", cmap="Blues", 
+        cbar=False
+     )
+    try:
+        plt.savefig(f'../../../figures/{model_name}_cm_{target_name}_{pipe_name}_dscrt.png')
+    except:
+        pass
+        
+    return cm
 
 
 def plot_feature_importance(coefs, n=10, figure_size=(20, 15), target="_ "):
